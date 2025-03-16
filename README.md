960d84d901e72d371fac85dd422f7df9c017b390
Gabriel Cavalcante <gscavalcante@gmail.com>
2025-03-16 14:51:35 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# My NeoVim Configurations
+
+## Requirements
+
+- Neovim
+- Git
+- [NerdFont](https://www.nerdfonts.com/)
+
---
+++ init.lua
@@ -0,0 +1,38 @@
+-- Author: Gabriel Cavalcante
+-- Email: gscavalcante@gmail.com
+-- Blog: https://gcavalcante.com
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+
+-- Make line numbers default
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help "list"`
+--  and `:help "listchars"`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", "<cmd>echo 'Use h to move!!'<CR>")
+vim.keymap.set("n", "<right>", "<cmd>echo 'Use l to move!!'<CR>")
+vim.keymap.set("n", "<up>", "<cmd>echo 'Use k to move!!'<CR>")
+vim.keymap.set("n", "<down>", "<cmd>echo 'Use j to move!!'<CR>")
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require("config.lazy")
+
