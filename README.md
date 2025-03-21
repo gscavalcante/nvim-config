2f0e36e09dd18a3a573ef64bca15065b8923853e
Gabriel Cavalcante <gscavalcante@gmail.com>
2025-03-21 15:10:19 +0000

    Fix lsp config

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 - Neovim >= 0.10
 - Git
 - [NerdFont](https://www.nerdfonts.com/)
+- Node
 
 > https://github.com/neovim/neovim/issues/27951
 > Maybe you will face some problems with Ubuntu
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -2,7 +2,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -16,8 +16,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
-require("lazy").setup({
+require("lazy").setup {
   spec = {
+    "tpope/vim-sleuth", -- Detect tabstop and shiftwidgh automatically
     -- import your plugins
     { import = "plugins" },
   },
@@ -26,4 +27,4 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
-})
+}
